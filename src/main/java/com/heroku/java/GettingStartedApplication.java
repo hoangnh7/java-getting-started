91e8a9189cb03eb9d2c2d4bf273caa1b21327075
hoangnh7 <126394299+hoangnh7@users.noreply.github.com>
2023-08-15 15:41:22 +0900

    asd

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -71,7 +71,7 @@ public class GettingStartedApplication {
 //            byte[] fileOut = Files.readAllBytes(filePath.toPath());
             byte[] fileOut = ExcelHelper.excelExport(candidate, new FileInputStream(filePath), "EN");
             HttpHeaders headers = new HttpHeaders();
-            String fileName = "abc.xlssx";
+            String fileName = "abc.xlsx";
             headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
             headers.setContentDispositionFormData( fileName, fileName);
             return new ResponseEntity<>(fileOut, headers, HttpStatus.OK);
